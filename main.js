d7dbad35e2dce74a4213e1230f4add6e2a1f39ff
Jamal25-ia <76519029+Jamal25-ia@users.noreply.github.com>
2025-01-26 23:17:07 -0400

    Quite marketing

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2000)
-  .typeString('Hago diseños de publicidad y marketing.')
+  .typeString('Hago diseños de publicidad.')
   .pauseFor(200)
   .deleteChars(40)
   .typeString('Perito en Administracion de empresas.')
