0de88db19d03f4085590049f077940f87cd7472f
Jamal25-ia <76519029+Jamal25-ia@users.noreply.github.com>
2025-08-02 02:27:52 -0400

    COnocimientos

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ let app = document.getElementById('typewriter');
  
 let typewriter = new Typewriter(app, {
   loop: true,
-  delay: 75,
+  delay: 70,
 });
  
 typewriter
-  .pauseFor(2000)
+  //.pauseFor(2000)
   .typeString('Hago diseños de publicidad.')
   .pauseFor(200)
   .deleteChars(40)
